64c644941b68c4d7d66de0ebe7e8fa405d4698b5
HosokawaR <thin.river.jp@gmail.com>
2021-10-23 16:51:17 +0900

    add lint & formatrt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
-};
+  return <div>Hel lo world</div>
+}
 
-export default Home;
+export default Home
