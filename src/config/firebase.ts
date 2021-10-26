4fc859d951b03787c6223493e9b53f06c619fbf2
Keito <54165015+Tanesan@users.noreply.github.com>
2021-10-26 17:43:28 +0900

    Github login

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+firebase.initializeApp({
+  apiKey: "AIzaSyA_A5aMoBBSMk6k9LWIB5t8lAB7DFuFMwE",
+  authDomain: "jphacks-2021.firebaseapp.com",
+  projectId: "jphacks-2021",
+  storageBucket: "jphacks-2021.appspot.com",
+  messagingSenderId: "325023986059",
+  appId: "1:325023986059:web:05119c17be9ecf5084ff8e"
+});
+
+export default firebase;
