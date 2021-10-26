cd68cdcb03a0e36963160145e27d794505903e34
nishidayoshikatsu <nsd24technology@gmail.com>
2021-10-26 19:13:00 +0900

    :lipstick: add footer and update codesandbox.

---
+++ component/Footer.tsx
@@ -0,0 +1,24 @@
+import { VFC } from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+
+const Footer: VFC = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style={{ backgroundColor: '#FFFFFF' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button style={{ backgroundColor: '#F87896', color: '#FFFFFF' }}>
+            提出
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
+
+export default Footer
--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,18 +1,22 @@
 import type { NextPage } from 'next'
 import { VFC } from 'react'
 import Header from '../component/Header'
+import Footer from '../component/Footer'
 
 const Submit: VFC = () => {
   return (
     <div>
       <Header />
-      <iframe
-        src="https://codesandbox.io/embed/frontcoder-sample1-5z2x6?fontsize=14&hidenavigation=1&theme=light"
-        className="iframe-coding"
-        title="FrontCoder-sample1"
-        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-      ></iframe>
+      <main>
+        <iframe
+          src="https://codesandbox.io/embed/frontcoder-sample1-zsck5?fontsize=14&hidenavigation=1&theme=light"
+          className="iframe-coding"
+          title="FrontCoder-sample1"
+          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+          sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+        ></iframe>
+      </main>
+      <Footer />
     </div>
   )
 }
