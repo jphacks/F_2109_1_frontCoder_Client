3679cb53e95365226dd2c7f795314e5a22b2f8ab
nishidayoshikatsu <nsd24technology@gmail.com>
2021-10-27 06:36:26 +0900

    :lipstick: codingとpreviewのUIを追加

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -4,16 +4,28 @@ import { VFC } from 'react'
 import DescriptionCard from '../component/DescriptionCard'
 import Header from '../component/Header'
 import Footer from '../component/Footer'
-import { AppBar } from '@mui/material'
+import { AppBar, Box } from '@mui/material'
 import Codesandbox from '../component/Codesandbox'
+import SampleView from '../component/SampleView'
 
 const Submit: VFC = () => {
   return (
     <div>
       <Header />
       <main>
-        {/*<DescriptionCard />*/}
-        <Codesandbox />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            p: 1,
+            m: 1,
+            height: '82vh',
+          }}
+        >
+          {/*<DescriptionCard />*/}
+          <Codesandbox />
+          <SampleView />
+        </Box>
       </main>
       <Footer />
     </div>
