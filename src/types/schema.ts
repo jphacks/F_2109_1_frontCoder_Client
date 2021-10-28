3433f2727ed18c1d9291f3138fd90866b953e329
nishidayoshikatsu <nsd24technology@gmail.com>
2021-10-28 17:04:14 +0900

    :label: add db schema.

---
+++ src/types/schema.ts
@@ -0,0 +1,20 @@
+export type FileType = {
+  fileName: string;
+  code: string;
+};
+
+export type Dir = {
+  dirName: string;
+  file: FileType[];
+  dir: Dir[] | undefined;
+};
+
+export type Problem = {
+  id: number;
+  title: string;
+  discription: string;
+  image: string;
+  source_code: Dir;
+  created_at: Date;
+  updated_at: Date;
+};
