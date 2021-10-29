56653e403290a7599508d69cda8b8b0d02702597
nishidayoshikatsu <nsd24technology@gmail.com>
2021-10-30 04:28:41 +0900

    :hammer: 問題ID対応

--- src/components/Codesandbox.tsx
+++ src/components/Codesandbox.tsx
@@ -1,13 +1,21 @@
 import { VFC } from 'react'
+import { useLocation } from 'react-router'
+
+import switchID from '../utils/switchID'
 
 const Codesandbox: VFC = () => {
-  console.info()
+  const path = useLocation()
+  const id: number = path.pathname.slice(-8, -6)
+  console.info(path)
+  console.info(id)
 
   return (
     <iframe
-      src="https://codesandbox.io/embed/wen-ti-1-m2z2x?fontsize=14&hidenavigation=1&theme=light"
+      src={`https://codesandbox.io/embed/${switchID(
+        1
+      )}?fontsize=14&hidenavigation=1&theme=light`}
       className="iframe-coding"
-      title="FrontCoder 問題1"
+      title={`FrontCoder 問題${id + 1}`}
       allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
       sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
     ></iframe>
